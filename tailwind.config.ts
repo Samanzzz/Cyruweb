fd22e9791c17aac26cc05a05b7ee38629c44d188
Saman Khadivar <samankhbusiness@gmail.com>
2025-07-14 06:54:18 +0000

    the strip is too fast make it slow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -96,8 +96,8 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'marquee': 'marquee 50s linear infinite',
-        'marquee2': 'marquee2 50s linear infinite',
+        'marquee': 'marquee 150s linear infinite',
+        'marquee2': 'marquee2 150s linear infinite',
       },
     },
   },
