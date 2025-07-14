263a86decea17eb2c6211478a1ad4c2f310ef9bd
Saman Khadivar <samankhbusiness@gmail.com>
2025-07-14 06:11:19 +0000

    it doesnt show

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,12 @@ export const metadata: Metadata = {
   title: "CyruTech Capital",
   description: "Pioneering Venture Capital for Groundbreaking Tech Innovations",
   icons: {
-    icon: "https://raw.githubusercontent.com/Samanzzz/Cyruweb/master/assets/images/logo/Cy-logo.png",
+    icon: [
+      {
+        url: 'https://raw.githubusercontent.com/Samanzzz/Cyruweb/master/assets/images/logo/Cy-logo.png',
+        href: 'https://raw.githubusercontent.com/Samanzzz/Cyruweb/master/assets/images/logo/Cy-logo.png',
+      },
+    ],
   },
 };
 
