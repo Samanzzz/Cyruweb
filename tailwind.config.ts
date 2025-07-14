b74bfc6e25e3145aa89ad703a1d29088af129f45
Saman Khadivar <samankhbusiness@gmail.com>
2025-07-14 06:54:53 +0000

    make it wayyy slower

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -96,8 +96,8 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'marquee': 'marquee 150s linear infinite',
-        'marquee2': 'marquee2 150s linear infinite',
+        'marquee': 'marquee 250s linear infinite',
+        'marquee2': 'marquee2 250s linear infinite',
       },
     },
   },
